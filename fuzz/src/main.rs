c7085c25f372546e82b34d4154aabb03b12279c1
ebell495 <ericbell495@gmail.com>
2022-09-29 21:50:32 -0700

    Add more hashes to the fuzzing harness

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -23,21 +23,37 @@ fn main() {
 
 
 fn fuzz_target(data: &[u8]) {
-    if data.len() > 1 {
+    if data.len() > 8 + 1 {
         let opt = data[0];
-        let fuzz_data = &data[1..];
+        let seed = unsafe{std::ptr::read(&data[1])} as u64;
+        let fuzz_data = &data[9..];
         match opt {
             0=> {
-                city::hash128(fuzz_data);
+                city::hash128_with_seed(fuzz_data, seed.into());
             },
             1=> {
-                farm::hash128(fuzz_data);
+                farm::hash128_with_seed(fuzz_data, seed.into());
             },
             2=> {
-                highway::hash128(fuzz_data);
+                highway::hash128_with_seed(fuzz_data, [seed, seed, seed, seed]);
             },
             3=> {
-                komi::hash64(fuzz_data);
+                komi::hash64_with_seed(fuzz_data, seed);
+            },
+            4=> {
+                metro::hash128_with_seed(fuzz_data, seed.try_into().expect("Fuzzing Issue"));
+            },
+            5=> {
+                murmur3::hash128_with_seed(fuzz_data, seed.try_into().expect("Fuzzing Issue"));
+            },
+            6=> {
+                spooky::hash128_with_seed(fuzz_data, seed.into());
+            },
+            7=> {
+                xxh3::hash128_with_seed(fuzz_data, seed);
+            },
+            8=> {
+                umash::hash128_with_seed(fuzz_data, seed);
             },
             _=> (),
         }
